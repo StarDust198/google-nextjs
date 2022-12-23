451f2e6c016d4148a57f45c743ba8ac20acd93f5
Sergei Zhilinskij <roadtomars2030@gmail.com>
2022-12-23 16:26:59 +0400

    data from google API

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,7 +1,12 @@
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
 import Head from 'next/head';
+import { ParsedUrlQuery } from 'node:querystring';
 import { SearchHeader } from '../components/SearchHeader/SearchHeader';
+import response from '../response.json';
 
-function Search() {
+function Search({ results }: { results: any }) {
+  console.log(results);
+
   return (
     <>
       <Head>
@@ -18,3 +23,24 @@ function Search() {
 }
 
 export default Search;
+
+export const getServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext<ParsedUrlQuery>
+) => {
+  const mock = true;
+
+  const data = mock
+    ? response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${
+          process.env.API_KEY
+        }&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${
+          context.query.searchType && '&searchType=image'
+        }`
+      ).then((response) => response.json());
+  return {
+    props: {
+      results: data,
+    },
+  };
+};
